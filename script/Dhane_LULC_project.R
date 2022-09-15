2e21b60ec59854c6d77db0bb2e64fe07a11936d6
faresdhane <fs.dhane@gmail.com>
2022-09-15 15:44:47 -0400

    Addinf a new README file

--- script/Dhane_LULC_project.R
+++ script/Dhane_LULC_project.R
@@ -5,7 +5,7 @@ library(tidyverse)
 dir.create("data/") #create data folder
 dir.create("script/") #Create script folder
 
-#Upload data from local drive
+# Upload data from local drive
 data.path <- "D:/Documents/LDP/Productivity and Reproducibility/Data_tables_and_maps/Tables/"
 myfiles <- list.files(path = data.path, pattern = "*.csv", full.names = TRUE)
 
@@ -19,4 +19,25 @@ data.list <- list2env(
     read_csv), 
   envir = .GlobalEnv)
 
-#select tables
+# select tables
+Dhane_lulc_classes_code <- data.list$D..Documents.LDP.Productivity.and.Reproducibility.Data_tables_and_maps.Tables.Dhane_lulc_classes_code
+Dhane_lulc_year_2000 <- data.list$D..Documents.LDP.Productivity.and.Reproducibility.Data_tables_and_maps.Tables.Dhane_lulc_year_2000
+Dhane_lulc_year_2020 <- data.list$D..Documents.LDP.Productivity.and.Reproducibility.Data_tables_and_maps.Tables.Dhane_lulc_year_2020
+Dhane_metadata <- data.list$D..Documents.LDP.Productivity.and.Reproducibility.Data_tables_and_maps.Tables.Dhane_metadata
+
+# list tables
+myList <- list(Dhane_lulc_classes_code = Dhane_lulc_classes_code, 
+               Dhane_lulc_year_2000 = Dhane_lulc_year_2000,
+               Dhane_lulc_year_2020 = Dhane_lulc_year_2020,
+               Dhane_metadata = Dhane_metadata)
+
+# Set path
+path.out <- "D:/Documents/LDP/Productivity and Reproducibility/Git/LDP-lulc_nb_2021-Dhane/data"
+setwd(path.out)
+
+# Save 
+for(i in names(myList)){
+  write.csv(myList[[i]], paste0(i,".csv"))
+}
+
+#Read data from Git "data folder"
