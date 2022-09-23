369153abb73db267efe13bdcc0a2e8b36e2d387d
faresdhane <fs.dhane@gmail.com>
2022-09-23 15:28:14 -0400

    Script to clean the data

---
+++ script/Data cleaning.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+#Read the data
+getwd() # to verify my WD
+list(getwd())
+
+data.path <- ("D:/Documents/LDP/Productivity and Reproducibility/Git/LDP-lulc_nb_2021-Dhane/data/raw")
+myfiles <- list.files(path = data.path, pattern = "*.csv", full.names = TRUE)
+
+# import all tables as separate data frames, remove file path and file extensions (.csv)
+data.list <- list2env(
+  lapply(
+    setNames(myfiles, 
+             make.names(
+               gsub(".*1_", "", 
+                    tools::file_path_sans_ext(myfiles)))), 
+    read_csv), 
+  envir = .GlobalEnv)
+
+# select tables
+Dhane_lulc_classes_code <- data.list$D..Documents.LDP.Productivity.and.Reproducibility.Data_tables_and_maps.Tables.Dhane_lulc_classes_code
+Dhane_lulc_year_2000 <- data.list$D..Documents.LDP.Productivity.and.Reproducibility.Data_tables_and_maps.Tables.Dhane_lulc_year_2000
+Dhane_lulc_year_2020 <- data.list$D..Documents.LDP.Productivity.and.Reproducibility.Data_tables_and_maps.Tables.Dhane_lulc_year_2020
+Dhane_metadata <- data.list$D..Documents.LDP.Productivity.and.Reproducibility.Data_tables_and_maps.Tables.Dhane_metadata
+
+# list tables
+myList <- list(Dhane_lulc_classes_code = Dhane_lulc_classes_code, 
+               Dhane_lulc_year_2000 = Dhane_lulc_year_2000,
+               Dhane_lulc_year_2020 = Dhane_lulc_year_2020,
+               Dhane_metadata = Dhane_metadata)
+
+# Data exploration
+
